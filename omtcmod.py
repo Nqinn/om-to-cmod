17f0208ec7c00bfc92393ce7d8514adea985a94b
Nain <76272025+Nqinn@users.noreply.github.com>
2023-05-13 05:40:52 +0700

    Update omtcmod.py

--- omtcmod.py
+++ omtcmod.py
@@ -2,11 +2,11 @@
 print('Hi, this program will convert your osu!mania scroll speed to etterna scroll speed')
 
 #asking for your data
-osu_mania_sp = int(input('\nPlease input your osu!mania scroll speed: '))
+osu_mania_sp = int(input('\nPlease input your osu!mania Scroll Speed: '))
 etterna_recep = int(input('\nPlease input your Etterna Receptor Size: '))
 
 #calculating and printing the results
 CMod = osu_mania_sp * 3200 / etterna_recep
 print('Your Etterna CMod is', CMod)
 
-next_calculation = input ('Press Enter to Close the Program: ')
+next_calculation = input ('Type Enter to Close the Program: ')
