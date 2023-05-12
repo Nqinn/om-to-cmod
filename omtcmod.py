5a7d3a64450e63b802a3b96b9688ad58b08f2b95
Nain <76272025+Nqinn@users.noreply.github.com>
2023-05-12 21:35:58 +0700

    Add files via upload

---
+++ omtcmod.py
@@ -0,0 +1,12 @@
+#welcome screen
+print('Hi, this program will convert your osu!mania scroll speed to etterna scroll speed')
+
+#ask for your data
+osu_mania_sp = int(input('\nPlease input your osu!mania scroll speed: '))
+etterna_recep = int(input('\nPlease input your Etterna Receptor Size: '))
+
+#calculating and printing the results
+CMod = osu_mania_sp * 3200 / etterna_recep
+print('Your Etterna CMod is', CMod)
+
+next_calculation = input ('Type "Close" and Enter to Close the Program: ')
