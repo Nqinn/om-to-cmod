197efb0d49a3875f9f25ef4a2424436d66768141
Evening <johnchangqi@hotmail.com>
2023-06-12 11:23:55 +0800

    Wrap code with Streamlit

--- omtcmod.py
+++ omtcmod.py
@@ -1,12 +1,15 @@
+import streamlit as st
 #welcome screen
 print('Hi, this program will convert your osu!mania scroll speed to etterna scroll speed')
 
 #asking for your data
-osu_mania_sp = int(input('\nPlease input your osu!mania Scroll Speed: '))
-etterna_recep = int(input('\nPlease input your Etterna Receptor Size: '))
+osu_mania_sp = st.number_input('\nPlease input your osu!mania Scroll Speed: ',
+                               min_value=1, max_value=40, step=1)
+etterna_recep = st.number_input('\nPlease input your Etterna Receptor Size: ',
+                               min_value=1, max_value=10000, step=10)
 
 #calculating and printing the results
 CMod = osu_mania_sp * 3200 / etterna_recep
-print('Your Etterna CMod is', CMod)
+st.text(f'Your Etterna CMod is {CMod}')
 
-next_calculation = input ('Type Enter to Close the Program: ')
+# next_calculation = input ('Type Enter to Close the Program: ')
